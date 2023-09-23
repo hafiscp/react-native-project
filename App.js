0ed57720770a1aa91e3f29dea615efc92a9eb688
hafiscp <cphafis3@gmail.com>
2023-09-23 14:41:03 +0530

    Sample - React Native - 019.Touchables

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Button Pressed");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.texts} onPress={handlePress}>
+        Hello World !
+      </Text>
+      {/* Touchables Starts */}
+      <TouchableWithoutFeedback onPress={() => console.log("Image Tapped")}>
+        <Image style={styles.img} source={require("./assets/icon.png")} />
+      </TouchableWithoutFeedback>
+      <TouchableOpacity onPress={() => console.log("Image Tapped")}>
+        <Image style={styles.img} source={require("./assets/icon.png")} />
+      </TouchableOpacity>
+      <TouchableHighlight onPress={() => console.log("Image Tapped")}>
+        <Image style={styles.img} source={require("./assets/icon.png")} />
+      </TouchableHighlight>
+      <TouchableNativeFeedback onPress={() => console.log("Image Tapped")}>
+        <View
+          style={{ width: 100, height: 70, backgroundColor: "tomato" }}
+        ></View>
+      </TouchableNativeFeedback>
+      {/* Touchables Ends */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  texts: {
+    padding: 10,
+    color: "white",
+    backgroundColor: "red",
+    borderRadius: 100,
+  },
+  img: {
+    height: 100,
+    width: 100,
   },
 });
