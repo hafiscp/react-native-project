09a9627b56be8e4d41a40f2990cf957307d4dffb
hafiscp <cphafis3@gmail.com>
2023-11-20 23:48:23 +0530

    24 - Dimensions

--- App.js
+++ App.js
@@ -5,12 +5,23 @@ import {
   SafeAreaView,
   Button,
   Platform,
+  View,
+  Dimensions,
 } from "react-native";
 
 export default function App() {
+  console.log(Dimensions.get("window"));
+  console.log(Dimensions.get("screem"));
+  // Dimensions returns the dimensions of screen or window. It has the width, height, and scale factor of the device.
   return (
     <SafeAreaView style={styles.container}>
-      <Button title="Click Me !" />
+      <View
+        style={{
+          backgroundColor: "dodgerBlue",
+          width: "50%",
+          height: 70,
+        }}
+      ></View>
     </SafeAreaView>
   );
 }
@@ -19,6 +30,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "black",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 100,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
---
+++ notes.md
@@ -0,0 +1,3 @@
+### 20th November 2023
+- Physical Pixels = Density-Independent Pixels * Scale Factor
+-   
