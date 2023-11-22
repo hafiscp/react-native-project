b8b791ac754e7288827d157ab2062835dd2199d9
hafiscp <cphafis3@gmail.com>
2023-11-22 23:53:22 +0530

    32 - Positioning of View

--- App.js
+++ App.js
@@ -40,6 +40,9 @@ export default function App() {
           backgroundColor: "gold",
           width: 100,
           height: 100,
+          top: 0,
+          left: 0,
+          position: "absolute",
         }}
       />
       <View
