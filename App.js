31eb3889de1a49c9fb368aa869a79ab06396251b
hafiscp <cphafis3@gmail.com>
2023-11-22 23:48:18 +0530

    31 - flexBasis, flexGrow, flexShrink

--- App.js
+++ App.js
@@ -23,13 +23,15 @@ export default function App() {
         justifyContent: "center", // Main - Primary Axis
         alignItems: "center", // Secondary Axis
         alignContent: "center", // Entire Content
-        flexWrap: "wrap",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: 100,
+          width: 100, //set the width to 300
+          // flexBasis: 100, // Alike flex. Value will be 100.
+          // flexGrow: 1, // If side is empty, it'll automatically fill with the existing view.
+          // flexShrink: 1, // If the view is in overflow, the flexShrink will be restrict the growth of overflowing view.
           height: 100,
         }}
       />
@@ -43,20 +45,6 @@ export default function App() {
       <View
         style={{
           backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "aqua",
           width: 100,
           height: 100,
         }}
