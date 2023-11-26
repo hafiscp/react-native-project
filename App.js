099a78721695ca91340a3cf41ba216d886fae6b8
hafiscp <cphafis3@gmail.com>
2023-11-27 00:26:25 +0530

    33 - Exercise Try

--- App.js
+++ App.js
@@ -17,41 +17,34 @@ export default function App() {
   return (
     <View
       style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row", // Horizontal
-        justifyContent: "center", // Main - Primary Axis
-        alignItems: "center", // Secondary Axis
-        alignContent: "center", // Entire Content
+        flex: 2,
+        backgroundColor: "grey",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center",
       }}
     >
       <View
         style={{
-          backgroundColor: "dodgerblue",
-          width: 100, //set the width to 300
-          // flexBasis: 100, // Alike flex. Value will be 100.
-          // flexGrow: 1, // If side is empty, it'll automatically fill with the existing view.
-          // flexShrink: 1, // If the view is in overflow, the flexShrink will be restrict the growth of overflowing view.
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
+          backgroundColor: "red",
           width: 100,
           height: 100,
+          position: "absolute",
           top: 0,
           left: 0,
-          position: "absolute",
         }}
-      />
+      ></View>
       <View
         style={{
-          backgroundColor: "tomato",
+          backgroundColor: "yellow",
           width: 100,
           height: 100,
+          position: "absolute",
+          top: 0,
+          right: 0,
         }}
-      />
+      ></View>
     </View>
   );
 }
