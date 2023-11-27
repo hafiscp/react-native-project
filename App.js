c9c2e043c47f869786353ed8fbfe6dc516cb66c8
hafiscp <cphafis3@gmail.com>
2023-11-27 15:00:37 +0530

    Inprogress - From MAC

--- App.js
+++ App.js
@@ -1,50 +1,11 @@
 import React from "react";
 import {
   StyleSheet,
-  StatusBar,
-  SafeAreaView,
-  Button,
-  Platform,
-  View,
-  Dimensions,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+} from 'react-native'
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 2,
-        backgroundColor: "grey",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        alignContent: "center",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "red",
-          width: 100,
-          height: 100,
-          position: "absolute",
-          top: 0,
-          left: 0,
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "yellow",
-          width: 100,
-          height: 100,
-          position: "absolute",
-          top: 0,
-          right: 0,
-        }}
-      ></View>
-    </View>
+   <WelcomeScreen/>
   );
 }
