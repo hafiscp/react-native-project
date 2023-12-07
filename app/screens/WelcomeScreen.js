3597f05e59c1c92140ceb1ccc0551139a66a11ef
hafiscp <cphafis3@gmail.com>
2023-12-07 17:23:56 +0530

    34 - Exercise - from Mac

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, View, Text } from 'react-native';
 
-function WelcomeScreen(props) {
+function WelcomeScreen() {
     return (
         <ImageBackground style={styles.background} source={require('../assets/bg.jpg')}>
-
+            <View style={styles.loginContainer}>
+                <ImageBackground source={require('../assets/splash.png')} style={styles.logo}></ImageBackground>
+                <Text>The Perfect Place for App Development</Text>
+            </View>
             <View style={styles.loginButton}></View>
+
         </ImageBackground>
     );
 }
@@ -13,12 +17,22 @@ function WelcomeScreen(props) {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center"
     },
-    loginButton : {
-        width : "100%",
-        height:70,
-        backgroundColor:"dodgerblue",
-
+    logo:{
+        width:"100%",
+        height:"100%",
+    },
+    loginContainer:{
+        position: "absolute",
+        top: 100,
+        alignItems:"center"
+    },
+    loginButton: {
+        width: "100%",
+        height: 100,
+        backgroundColor: "#ff0012",
     }
 })
 
