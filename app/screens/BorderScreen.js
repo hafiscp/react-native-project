caf20063488a56bd9979282212e42ebd123830df
hafiscp <cphafis3@gmail.com>
2023-12-09 22:55:37 +0530

    39 - Shadows - From Lap

--- app/screens/BorderScreen.js
+++ app/screens/BorderScreen.js
@@ -14,12 +14,12 @@ const styles = StyleSheet.create({
   borders: {
     width: 100,
     height: 100,
-    backgroundColor: colors.black,
-    borderWidth: 10,
-    borderColor: colors.secondary,
-    borderRadius: 10,
-    borderTopLeftRadius: 100,
-    borderBottomRightRadius: 100,
+    backgroundColor: colors.primary,
+    shadowColor: colors.primary, //iOS
+    shadowOffset: { width: 100, height: 100 }, //iOS
+    shadowOpacity: 1, //iOS
+    shadowRadius: 10, //iOS
+    elevation: 20, //Android
   },
   container: {
     justifyContent: "center",
